40f8d4e025b65da3e4afc03ce0e28918e668abc5
coleexz <williamcole@unitec.edu>
2024-10-27 20:45:10 -0600

    Terminado primer video

--- Shell.py
+++ Shell.py
@@ -2,7 +2,7 @@ import Basic
 
 while True:
     text = input('Algo >')
-    result, error = Basic.run(text)
-    
+    result, error = Basic.run('<stdin>',text)
+
     if error: print(error.as_string())
     else: print(result)
